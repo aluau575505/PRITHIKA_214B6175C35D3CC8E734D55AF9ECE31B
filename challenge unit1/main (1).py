7f3522a582605df68bda0ae8f04f0bc17edddce5
aluau575505 <144681364+aluau575505@users.noreply.github.com>
2023-09-11 17:57:52 +0530

    Add files via upload

--- challenge unit1/main (1).py
+++ challenge unit1/main (1).py
@@ -1,10 +1,18 @@
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
+#leap year
 
-number=int(input("enter a value:"))
-res=fact_rec(number)
+"""
+year % 4 == 0 &
+year % 100 != 0 /
+year % 400 == 0
 
-print("the factorial of{}is{}.".format(number,res))
+"""
+def isleapyear(year):
+ if(year % 4 == 0 and year % 100!= 0) or year % 400 == 0:
+  return True
+ else:
+  return False
+year=int(input("enter a year:"))
+if isleapyear(year):
+  print('{} isa leap year.'.format(year))
+else:
+  print('{}is not a leapyear:.'.format(year))
